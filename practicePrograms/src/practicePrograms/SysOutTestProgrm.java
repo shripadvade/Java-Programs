b5990cf8834610c638719b212976c2e321da28b9
shripad <shripad@LAPTOP-VS8QSRGL>
2024-01-16 06:10:49 +0530

    Sysout sample checkin

---
+++ practicePrograms/src/practicePrograms/SysOutTestProgrm.java
@@ -0,0 +1,10 @@
+package practicePrograms;
+
+public class SysOutTestProgrm {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Class:SysOutTestProgrm method:main First Sysout");
+	}
+
+}
